8712afe99d75b219cdb969e756d449a47c342c27
clf <chenlufeng@outlook.com>
2023-10-13 00:25:15 +0800

    step8 invoker

---
+++ src/main/java/com/chen/mybatis/reflection/invoker/Invoker.java
@@ -0,0 +1,11 @@
+package com.chen.mybatis.reflection.invoker;
+
+/**
+ * 调用者
+ */
+public interface Invoker {
+
+    Object invoke(Object target, Object[] args) throws Exception;
+
+    Class<?> getType();
+}
---
+++ src/main/java/com/chen/mybatis/reflection/invoker/MethodInvoker.java
@@ -0,0 +1,14 @@
+package com.chen.mybatis.reflection.invoker;
+
+public class MethodInvoker implements Invoker{
+
+    @Override
+    public Object invoke(Object target, Object[] args) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Class<?> getType() {
+        return null;
+    }
+}
