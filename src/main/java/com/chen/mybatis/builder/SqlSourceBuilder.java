36d2f27493ab25259aaa821f8aa8c319e80560b8
clf <chenlufeng@outlook.com>
2023-12-11 00:00:27 +0800

    step9 sql 源码

---
+++ src/main/java/com/chen/mybatis/builder/SqlSourceBuilder.java
@@ -0,0 +1,7 @@
+package com.chen.mybatis.builder;
+
+public class SqlSourceBuilder extends BaseBuilder{
+
+
+
+}
--- src/main/java/com/chen/mybatis/scripting/defaults/RawSqlSource.java
+++ src/main/java/com/chen/mybatis/scripting/defaults/RawSqlSource.java
@@ -1,15 +1,36 @@
 package com.chen.mybatis.scripting.defaults;
 
+import com.chen.mybatis.mapping.BoundSql;
+import com.chen.mybatis.mapping.SqlSource;
 import com.chen.mybatis.scripting.xmltags.MixedSqlNode;
 import com.chen.mybatis.session.Configuration;
 
 /**
  * 原始SQL 源码, 比 DynamicSqlSource 动态SQL 处理快
  */
-public class RawSqlSource {
+public class RawSqlSource implements SqlSource{
 
-
+    private final SqlSource sqlSource;
 
     public RawSqlSource(Configuration configuration, MixedSqlNode rootSqlNode, Class<?> parameterType) {
+        this(configuration,getSql(configuration,rootSqlNode),parameterType);
+    }
+
+
+    public RawSqlSource(Configuration configuration,String sql, Class<?> parameterType) {
+        SqlSourceBuilder sqlSourceBuilder = new SqlSourceBuilder(configuration);
+
+        sqlSource = sqlSourcePara
+    }
+
+
+
+    @Override
+    public BoundSql getBoundSql(Object parameterObject) {
+        return null;
+    }
+
+    private static MixedSqlNode getSql(Configuration configuration, MixedSqlNode rootSqlNode) {
+        return null;
     }
 }
